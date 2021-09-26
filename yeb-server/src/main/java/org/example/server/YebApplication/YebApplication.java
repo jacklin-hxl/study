a0e0d004e5c44aef3db8942f216775e575c968ab
Xuelin <19946174874@163.com>
2021-09-27 00:00:00 +0800

    first commit

---
+++ yeb-server/src/main/java/org/example/server/YebApplication/YebApplication.java
@@ -0,0 +1,12 @@
+package org.example.server.YebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class YebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YebApplication.class, args);
+    }
+}
